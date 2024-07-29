4373f0c17267f37902f0c92ca1428eb35ac681ed
murilopbc <murilopoltronieri3@gmail.com>
2024-07-29 09:57:00 -0300

    path swagger

--- src/main/java/com/murilo/auth/AuthApplication.java
+++ src/main/java/com/murilo/auth/AuthApplication.java
@@ -8,6 +8,7 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+		// Path Swagger  http://localhost:8081/swagger-ui/index.html#/
 	}
 
 }
